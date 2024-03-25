72c37f23bba537703cce70a41198f524058bda6f
osenvesen <147384976+osenvesen@users.noreply.github.com>
2024-03-25 18:18:16 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,11 @@
 #include "fun.h"
 
 int main() {
+   const char *input = "fghfijghd Vfpjm 5739 78678578 Aghfgmb fgkmognopgnnonrojn";
+    
+    std::cout << "Количество слов, не содержащих цифр: " << faStr1(input) << std::endl;
+    std::cout << "Количество слов, начинающихся на заглавную латинскую букву, а среди других символов этих слов только латинские строчные буквы: " << faStr2(input) << std::endl;
+    std::cout << "Средняя длина слова в строке: " << faStr3(input) << std::endl;
+    
     return 0;
 }
