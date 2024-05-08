94bb1d0efeaeda7c8cac8827395c924f2fd55513
osenvesen <147384976+osenvesen@users.noreply.github.com>
2024-05-08 13:14:02 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,87 +1,86 @@
-// Copyright 2022 UNN-IASR
 #include "fun.h"
 #include <iostream>
 #include <cstring>
 #include <cctype>
 
 unsigned int faStr1(const char* str) {
-unsigned int count = 0;
-bool hasDigit = false;
-int i = 0;
+    unsigned int count = 0;
+    bool hasDigit = false;
+    int i = 0;
 
-while (str[i] != '\0') {
-if (isalnum(str[i]) && isdigit(str[i])) {
-hasDigit = true;
-}
-if (!isalnum(str[i]) || str[i + 1] == '\0') {
-if (!hasDigit) {
-count++;
-}
-hasDigit = false;
-}
-i++;
-}
+    while (str[i] != '\0') {
+        if (isalnum(str[i]) && isdigit(str[i])) {
+            hasDigit = true;
+        }
+        if (!isalnum(str[i]) || str[i + 1] == '\0') {
+            if (!hasDigit) {
+                count++;
+            }
+            hasDigit = false;
+        }
+        i++;
+    }
 
-return count;
+    return count;
 }
 
 unsigned int faStr2(const char* str) {
-unsigned int count = 0;
-int i = 0;
+    unsigned int count = 0;
+    int i = 0;
 
-while (str[i] != '\0') {
-if (isupper(str[i])) {
-i++;
-while (islower(str[i])) {
-i++;
-}
-if (!isalpha(str[i])) {
-count++;
-}
-}
-else {
-while (islower(str[i])) {
-i++;
-}
-}
-i++;
-}
+    while (str[i] != '\0') {
+        if (isupper(str[i])) {
+            i++;
+            while (islower(str[i])) {
+                i++;
+            }
+            if (!isalpha(str[i])) {
+                count++;
+            }
+        }
+        else {
+            while (islower(str[i])) {
+                i++;
+            }
+        }
+        i++;
+    }
 
-return count;
+    return count;
 }
 
 unsigned int faStr3(const char* str) {
-unsigned int totalLength = 0;
-unsigned int wordCount = 0;
-int i = 0;
+    unsigned int totalLength = 0;
+    unsigned int wordCount = 0;
+    int i = 0;
 
-while (str[i] != '\0') {
-if (isalnum(str[i])) {
-int wordLength = 0;
-while (isalnum(str[i])) {
-wordLength++;
-i++;
-}
-totalLength += wordLength;
-wordCount++;
-}
-else {
-i++;
-}
-}
+    while (str[i] != '\0') {
+        if (isalnum(str[i])) {
+            int wordLength = 0;
+            while (isalnum(str[i])) {
+                wordLength++;
+                i++;
+            }
+            totalLength += wordLength;
+            wordCount++;
+        }
+        else {
+            i++;
+        }
+    }
 
-return totalLength / wordCount;
+    return totalLength / wordCount;
 }
 
 int main() {
 
-setlocale(LC_ALL, "rus");
+    setlocale(LC_ALL, "rus");
 
-const char* input = "fghfijghd Vfpjm 5739 78678578 Aghfgmb fgkmognopgnnonrojn";
+    const char* input = "fghfijghd Vfpjm 5739 78678578 Aghfgmb fgkmognopgnnonrojn";
 
-std::cout « "Количество слов, не содержащих цифр: " « faStr1(input) « std::endl;
-std::cout « "Количество слов, начинающихся на заглавную латинскую букву, а среди других символов этих слов только латинские строчные буквы: " « faStr2(input) « std::endl;
-std::cout « "Средняя длина слова в строке: " « faStr3(input) « std::endl;
+    std::cout << "Количество слов, не содержащих цифр: " << faStr1(input) << std::endl;
+    std::cout << "Количество слов, начинающихся на заглавную латинскую букву, а среди других символов этих слов только латинские строчные буквы: " << faStr2(input) << std::endl;
+    std::cout << "Средняя длина слова в строке: " << faStr3(input) << std::endl;
 
-return 0;
+    return 0;
 }
