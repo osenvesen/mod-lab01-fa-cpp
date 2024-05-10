9485ca2c9e74c11aa99b6bca29e0bb78d5dd639e
osenvesen <147384976+osenvesen@users.noreply.github.com>
2024-05-10 09:40:24 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,12 @@
-// Copyright 2022 UNN-IASR
-#include <cctype>
-#include <cstring>
-#include <iostream>
-#include "fun.h"
+int main() {
 
-int main() 
-{
     setlocale(LC_ALL, "rus");
-    const char* input = "fghfijghd Vfpjm 5739";
-    std::cout << "Количество слов, не содержащих цифр: "
-              << faStr1(input) << std::endl;
-    std::cout << "Количество слов, начинающихся на заглавную латинскую букву, "
-              << "а среди других символов этих "
-              << "слов только латинские строчные буквы:"
-              << faStr2(input) << std::endl;
-    std::cout << "Средняя длина слова в строке: "
-              << faStr3(input) << std::endl;
+
+    const char* input = "fghfijghd Vfpjm 5739 78678578 Aghfgmb fgkmognopgnnonrojn";
+
+    std::cout << "Количество слов, не содержащих цифр: " << faStr1(input) << std::endl;
+    std::cout << "Количество слов, начинающихся на заглавную латинскую букву, а среди других символов этих слов только латинские строчные буквы: " << faStr2(input) << std::endl;
+    std::cout << "Средняя длина слова в строке: " << faStr3(input) << std::endl;
+
     return 0;
 }
